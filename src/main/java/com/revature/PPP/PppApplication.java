f7650ca7f6c99e7cfe5d44ecf976f52ea2da82aa
w314 <piroska804@revature.com>
2024-04-12 21:08:33 -0500

    Initial commit

---
+++ src/main/java/com/revature/PPP/PppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.PPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/PPP/PppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.PPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
